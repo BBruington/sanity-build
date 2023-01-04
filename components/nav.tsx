dd1b0ec4c034de3c68e6149579fbc1f7f4898a08
BIBruington <psychological_chemist@hotmail.com>
2023-01-04 16:44:59 -0500

    nav bar

---
+++ components/nav.tsx
@@ -0,0 +1,18 @@
+import Link from "next/link"
+
+function Nav() {
+  return (
+    <header>
+        <div>
+          <Link href="/">
+            <img src="https://links.papareact.com/yvf" 
+            alt="" />
+          </Link>
+
+        </div>
+        <div></div>
+    </header>
+  )
+}
+
+export default Nav

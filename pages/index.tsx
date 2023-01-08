b5fb84392bbabc92805a1c20be44dd39d2cd54a2
BIBruington <psychological_chemist@hotmail.com>
2023-01-07 22:57:15 -0500

    home page styles

--- pages/index.tsx
+++ pages/index.tsx
@@ -43,13 +43,18 @@ const Home = (props: Props) => {
         />
       </div>
       {/* posts */}
-      <div>
+      <div className='p-3 gap-3 grid grid-cols-1 sm:grid-cols-2 md:gap-6 md:p-6 lg:grid-cols-3 '>
         {posts.map(post => (
           <Link key={post._id} href={`/post/${post.slug.current}`}>
-            <div>
-              <img src={
-                urlFor(post.mainImage).url()!
-              } alt="" />
+            <div className='border rounded-lg group cursor-pointer overflow-hidden'>
+              <img className='h-60 w-full object-cover group-hover:scale-105 transition-transform duration-200 ease-in-out' src={urlFor(post.mainImage).url()!} alt="" />
+              <div className='flex justify-between p-5 bg-white'>
+                <div>
+                  <p className='test-lg font-bold'>{post.title}</p>
+                  <p className='text-xs'>{post.description} by {post.author.name}</p>
+                </div>
+                <img className='h-12 w-12 rounded-full' src={urlFor(post.author.image).url()!} alt="" />
+              </div>
             </div>
           </Link>
         ))}

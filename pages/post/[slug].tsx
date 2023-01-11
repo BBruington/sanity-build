c953376aaf935ee80c64a2277d14fccce0516e80
BIBruington <psychological_chemist@hotmail.com>
2023-01-10 23:40:53 -0500

    slug article structure

--- pages/post/[slug].tsx
+++ pages/post/[slug].tsx
@@ -1,4 +1,5 @@
 import { GetStaticProps } from "next";
+import PortableText from "react-portable-text";
 import Nav from "../../components/nav";
 import { sanityClient, urlFor } from "../../utils/sanity";
 import { Post } from "../../utils/typings";
@@ -17,8 +18,49 @@ function Post({post}: Props) {
           <Nav />
 
           <img className="w-full h-40 object-cover" src={urlFor(post.mainImage).url()} alt='' />
-          <article className="max-w-3x1">
-            <h1 className="text-3x1">{post.title}</h1>
+          <article className="max-w-3xl mx-auto p-5">
+
+            <h1 className="text-3xl mt-10 mb-3">{post.title}</h1>
+          
+            <h2 className="text-xl font-light text-gray-500 mb-2">{post.description}</h2>
+
+            <div className="flex items-center space-x-2">
+
+              <img className="h-10 w-10 rounded-full" src={urlFor(post.author.image).url()} alt='' />
+            
+              <p className="font-extralight text-sm">
+                Blog post by <span className="text-green-600">{post.author.name}</span> - Published at{" "} 
+              {new Date(post._createdAt).toLocaleString()}</p>
+            
+            </div>
+
+            <div className="mt-10">
+              <PortableText 
+                className=""
+                dataset={process.env.NEXT_PUBLIC_SANITY_DATASET!}
+                projectId={process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!}
+                content={post.body}
+                serializers={
+                  {
+                    h1: (props: any) => {
+                      <h1 className="text-2xl font-bold my-5" {...props}/>
+                    },
+                    h2: (props: any) => {
+                      <h1 className="text-xl font-bold my-5" {...props} />
+                    },
+                    li: ({ children }: any) => {
+                      <li className="ml-4 list-disc">{children}</li>
+                    },
+                    link: ({ href, children }: any) => {
+                      <a href={href} className="text-blue-500 hover:underline">
+                        {children}
+                      </a>
+                    },
+                  }
+                }
+              />
+            </div>
+
           </article>
         </main>
       </>
